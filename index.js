c4f98fb4be56a7a6c515ce7ddb3321eb7b6d97d8
Scout Cox <coxscout@gmail.com>
2022-08-01 12:29:13 -0400

    commit tree 6d0cce7cf8d5399fda2ea1395c0d6211a6093033

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    let uppercase=string.toUpperCase();
+    string=uppercase;
+    console.log(uppercase);
+}
+
+function logWhisper(string){
+    let lowercase=string.toLowerCase();
+    string=lowercase;
+    console.log(lowercase);
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase() === string){
+    return "I can't hear you!";
+    } if (string.toUpperCase() === string){
+    return "YES INDEED!";
+    } if (string === "Let's have dinner together!"){
+    return "I would love to!";
+    }
+}
